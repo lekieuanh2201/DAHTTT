b70cae0727520c62e0eb7263a6dfaefed5ebc71b
Phan Huu Khanh Duy <duyphk@neo.vn>
2023-05-30 11:41:33 +0700

    update workflow kafka2ES

--- elastic_search.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" --env ES_JAVA_OPTS="-Xmx300m" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.7.0
