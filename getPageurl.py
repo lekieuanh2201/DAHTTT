d0f624dfab248e81b5443dfa01ce0f22eb186c35
NguyenDrasp <datngobhlc@gmail.com>
2023-06-08 16:47:21 +0700

    getPagebyKey

---
+++ getPageurl.py
@@ -0,0 +1,58 @@
+from facebook_scraper import get_profile
+from playwright.sync_api import sync_playwright
+import time
+import ast
+
+def pagetiengviet(text):
+
+    ans = True
+    return ans
+
+def searchPagebyKey(keyword):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False, slow_mo=50)
+
+        context = browser.new_context(storage_state='state.json')
+        
+        page = context.new_page()
+
+        page.goto('https://facebook.com')
+        time.sleep(3)
+        page.goto('https://www.facebook.com/search/pages?q='+keyword)
+
+        for i in range(0,10):
+            page.mouse.wheel(0,15000)
+            time.sleep(1)
+        pages_infor = page.locator('xpath=/html/body/div[1]/div/div[1]/div/div[5]/div/div/div[3]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div/div[2]/div[1]').all()
+                                    
+        file = open('page.txt')                                  
+        for page_infor in pages_infor:
+            page_url = page_infor.locator('xpath=/div[1]/div/div[1]/span/div/a').get_attribute('href')
+            print(page_url)
+            page_brief = page_infor.locator('xpath=/div[1]/div/div[2]/span/span').text_content().split('·')
+            page_describe = page_infor.locator('xpath=div/div/div[3]/span/span').text_content()
+            score = 0
+            for infor in page_brief:
+                try:
+                    if ('người theo dõi' in infor):
+                        x = infor.split(' n')[0]
+                        if 'triệu' in x:
+                            score = float(x.split('\xa0')[0].replace(' ','').replace(',','.'))*1000000
+                        elif 'K' in x:
+                            score = float(x.split('\xa0')[0][1:-1].replace(' ','').replace(',','.'))*1000
+                    elif ('lượt thích' in infor):
+                        x = infor.split(' l')[0]
+                        if 'triệu' in x:
+                            score = float(x.split('\xa0')[0].replace(' ','').replace(',','.'))*1000000
+                        elif 'K' in x:
+                            score = float(x.split('\xa0')[0][1:-1].replace(' ','').replace(',','.'))*1000
+                except:
+                    print(page_brief)
+
+            if score > 100000:
+                if pagetiengviet(page_describe):
+                    file.writelines(page_url)
+                    
+        storage = context.storage_state(path="state.json")
+    
+searchPagebyKey('showbiz')
--- test.py
+++
@@ -1,6 +0,0 @@
-from facebook_scraper import get_posts
-
-posts = get_posts( post_urls=['https://www.facebook.com/879869793499217', 'https://www.facebook.com/beatvn.network/posts/pfbid035aK2fANWo9oCrWzGQj6RjZ7oEpJbyuHYYo2dakTKHxXBAJEAqhdVsPBaPGHdLHesl?__cft__[0]=AZXpeno7-uw8HZ4G0cM4YZ55IVbNZ_Z3QIyo6ziDHzcRE-xJv8Ybcunvet25NNsrNFUm6XGBYVIhwF4OmopftxEAv98YY6ggOl2ipRpzz_rSSixGMzPYJ0l3T5wdJ4asNpjy0WtXIsk2lXT27Guh7raq&__tn__=%2CO%2CP-R'] )
-
-for post in posts:
-    print(post['text'])
