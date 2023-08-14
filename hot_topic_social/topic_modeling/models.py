5c3126b3e3112349da28ff00662b1e1abd62c78f
Tran Quang Dai <tranquangdai7a@gmail.com>
2023-08-14 20:41:57 +0700

    apis and topic model function

--- hot_topic_social/topic_modeling/models.py
+++ hot_topic_social/topic_modeling/models.py
@@ -1,23 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    post_id = models.AutoField(primary_key=True)
-    content = models.TextField()
-    num_like = models.IntegerField()
-    date = models.DateTimeField()
+# class Post(models.Model):
+#     post_id = models.AutoField(primary_key=True)
+#     content = models.TextField()
+#     num_like = models.IntegerField()
+#     date = models.DateTimeField()
 
-    class Meta:
-        managed = True
+#     class Meta:
+#         managed = True
 
-    def __str__(self):
-        return self.content
+#     def __str__(self):
+#         return self.content
     
 
 class Topic(models.Model):
     topic_id = models.AutoField(primary_key=True)
     name = models.TextField()
     add_time = models.DateTimeField()
+    post_ids_list = models.TextField()
 
     class Meta:
         managed = True
@@ -26,19 +27,19 @@ class Topic(models.Model):
         return self.name
     
 
-class Topic_post(models.Model):
-    topic = models.OneToOneField(Topic, models.DO_NOTHING, primary_key=True)
-    post = models.ForeignKey(Post, models.DO_NOTHING)
+# class Topic_post(models.Model):
+#     topic = models.OneToOneField(Topic, models.DO_NOTHING, primary_key=True)
+#     post = models.ForeignKey(Post, models.DO_NOTHING)
 
-    class Meta:
-        managed = True
-        unique_together = (('topic', 'post'),)
+#     class Meta:
+#         managed = True
+#         unique_together = (('topic', 'post'),)
 
-    def get_topic_id(self):
-        return self.topic
+#     def get_topic_id(self):
+#         return self.topic
     
-    def get_post_id(self):
-        return self.post
+#     def get_post_id(self):
+#         return self.post
     
-    def __str__(self):
-        return self.topic + ' ' + self.post
+#     def __str__(self):
+#         return self.topic + ' ' + self.post
